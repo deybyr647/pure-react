44a498f9cc25223b50f9cfa43c8560dcc02c16e2
deybyr647 <deybyr647@gmail.com>
2020-07-02 20:37:02 -0400

    Made components reusable, & added Moment module

--- src/Tweet.js
+++ src/Tweet.js
@@ -1,60 +1,90 @@
 import React from 'react';
+import moment from 'moment';
 import './Tweet.css';
 
-let Avatar = () => {
+let Avatar = ({hash}) => {
+  let url = `https://www.gravatar.com/avatar/${hash}`
   return(
-    <img src='https://www.gravatar.com/avatar/nothing' 
+    <img src={url} 
     className='avatar' alt='avatar'/>
   );
 }
 
-let Message = () => {
+let Message = ({text}) => {
   return(
-    <div className='message'> Hey Y'all, it's Deyby</div>
+    <div className='message'> {text} </div>
   );
 }
 
-let Author = () => {
+let NameWithHandle = ({author}) => {
+  const {name, handle} = author;
   return(
     <span className='author'>
-      <span className='name'> Deyby </span>
-      <span className='handle'>@deybyr647</span>
+      <span className='name'>{name}</span>
+      <span className='handle'>@{handle}</span>
     </span>
   );
 }
 
-const Time = () => (
-  <span className='time'>3h ago</span>
-);
+const Time = ({time}) => {
+  const timeString = moment(time).fromNow();
+  
+  return(
+    <span className='time'>{timeString}</span>
+
+  )
+}
 
 const ReplyButton = () => (
   <i className='fa fa-reply reply-button'/>
 );
 
-const RetweetButton = () => (
+const RetweetButton = ({count}) => (
+  <span className='retweet-button'>
   <i className='fa fa-retweet retweet-button'/>
+  {getRetweetCount(count)}
+  </span>
 );
 
-const LikeButton = () => (
-  <i className='fa fa-heart like-button'/>
+let getRetweetCount = (count) => {
+  if(count > 0){
+    return(
+      <span className='retweet-count'>
+        {count}
+      </span>
+    );
+  } else{
+    return null;
+  }
+}
+
+const LikeButton = ({count}) => (
+  <span className='like-button'>
+    <i className='fa fa-heart like-button'/>
+    {count > 0 && 
+    <span className='like-count'>
+      {count}
+    </span>}
+  </span>
 );
 
 const MoreOptionsButton = () => (
   <i className='fa fa-ellipsis-h more-options-button'/>
 )
 
-let Tweet = () => {
+let Tweet = ({tweet}) => {
   return(
     <div className='tweet'>
-     <Avatar/>
+     <Avatar hash={tweet.gravatar}/>
 
       <div className='content'>
-        <Author/> <Time/>
-        <Message/>
+        <NameWithHandle author={tweet.author}/> 
+         <Time time={tweet.timestamp}/>
+        <Message text={tweet.message}/>
         <div className='buttons'>
           <ReplyButton/>
-          <RetweetButton/>
-          <LikeButton/>
+          <RetweetButton count={tweet.retweets}/>
+          <LikeButton count={tweet.likes}/>
           <MoreOptionsButton/>
         </div>  
       </div>
@@ -63,11 +93,51 @@ let Tweet = () => {
   );
 }
 
+let Tweet1 = {
+  message: 'Good morning, friends !',
+  gravatar: 'xyz',
+  author: {
+    handle: 'deybyr647',
+    name: 'Deyby'
+  },
+  likes: 2,
+  retweets: 3,
+  timestamp:'2020-07-02 12:45:07'
+}
+
+let Tweet2 = {
+  message: 'You cannot say Pop & 4get the Smoke',
+  gravatar: 'xyz',
+  author: {
+    handle: 'volumizing',
+    name: 'Divad'
+  },
+  likes: 5000,
+  retweets: 2000,
+  timestamp: '2019-08-25 15:30:18'
+}
+
+let Tweet3 = {
+  message: 'Fuck it, Perc 3 million time',
+  gravatar: 'xyz',
+  author: {
+    handle: '7ven',
+    name: 'Tax Fraud God'
+  },
+  likes:4000,
+  retweets: 1500,
+  timestamp: '2019-10-31 20:45:18'
+}
+
 class App extends React.Component{
   render(){
     return(
       <>
-        <Tweet />
+        <Tweet tweet={Tweet2}/>
+        <br/>
+        <Tweet tweet={Tweet1}/>
+        <br/>
+        <Tweet tweet={Tweet3}/>
       </>  
     );
   }
